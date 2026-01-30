712067ff23fe63aa926b9aaf37698383a1d72239
heitor.bleao <heitor.bleao@TIT0676445W11-1>
2026-01-29 22:12:01 -0300

    Adicionar arquivos de projeto.

---
+++ MundoAFora/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MundoAFora
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
